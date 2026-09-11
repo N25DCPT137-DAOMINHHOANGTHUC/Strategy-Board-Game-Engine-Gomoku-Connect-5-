ed0f18effae147235b88cb74da731a4b70dbae80
BaoThy-N25DCPT138 <n25dcpt138@student.ptithcm.edu.vn>
2026-09-11 21:24:24 +0700

    Add files via upload

---
+++ xmag2chieu1515.h
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std ;
+int main (){
+	//Nhap 
+ int a[10][10];
+  for (int i=0;i<10;i++) {
+   for (int j=0; j<10;j++){
+    cin >> a[i][j];
+} 
+}
+     //Xuat
+  for (int i=0;i<10;i++) {
+   for (int j=0; j<10;j++){
+    cout << a[i][j]<< " ";
+} 
+cout << endl;
+}   
+return 0; 
+} 
